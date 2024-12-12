ed558091a8d897f3b6d88124138760e3605e545e
Your Name <you@example.com>
2024-12-12 17:12:17 -0500

    Initial updatingall

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require ("express");
+const mongoose = require("mongoose");
+const userRouter = require("./routes/users.js");
+
+const app = express();
+const { PORT = 3001 } = process.env;
+
+
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("Connected to DB");
+})
+  .catch(console.error);
+
+app.use("/", userRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
