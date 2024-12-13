eb9b89c03b0930ec53c649f85c30f7824926e682
Your Name <you@example.com>
2024-12-13 16:50:02 -0500

    Updating connect to mongoDB

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 
 mongoose
-  .connect("mongodb://127.0.1:27017/wtwr_db")
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
   .then(() => {
     console.log("connected to DB");
   })
