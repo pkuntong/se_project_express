70e9ebae25f0f8157d431564806aec31796128b2
Your Name <you@example.com>
2025-02-27 13:31:38 -0500

    update

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db", () => {
   console.log("connected to DB", (e) => console.log("DB error", e));
